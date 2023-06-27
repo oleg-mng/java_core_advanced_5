a44e889efb0dcee85c84428d795e582af6a5a055
oleg <legik7127@gmail.com>
2023-06-27 16:50:00 +0600

    added class BackUpFolder

--- src/main/java/org/inwork/Program.java
+++ src/main/java/org/inwork/Program.java
@@ -3,6 +3,7 @@ package org.inwork;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,26 +11,39 @@ import static java.nio.file.Files.copy;
 
 public class Program {
     public static void main(String[] args) throws IOException {
+        /**
+         * write to file 9 values (from 0 to 3) using 3 bytes
+         */
 
         ThreeBytes threeBytes = new ThreeBytes();
         threeBytes.writeThreeBytesFile("threeBytes.txt");
 
-//        copyBackUpFiles(".");
+        /**
+         * copy files from folder to backup folder
+         */
+
+        copyBackUpFiles(".");
 
     }
 
     private static void copyBackUpFiles(String p) throws IOException {
         List<String> list = new ArrayList<>();
+        String k = ".";
         File path = new File(new File(".").getCanonicalPath());
         File[] dir = path.listFiles();
-        File dest = new File("./backup");
+//        File dest = new File("./backupT");
         for (int i = 0; i < dir.length; i++) {
             if (dir[i].isFile()) {
                 for (int j = 0; j < dir.length; j++) {
-                    Files.copy(dir[i].toPath(), dest.toPath());
+                    try {
+                        Files.copy(dir[i].toPath(), Path.of("./backupT"));
+                    } catch (IOException ex) {
+                        ex.getMessage();
+                    }
                 }
 
             }
         }
+
     }
 }
